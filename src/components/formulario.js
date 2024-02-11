e95a365b1591387f9691fbd61d77bca9e49c7dfb
Willy Christian <will@MacBook-Pro-de-Willy.local>
2024-02-11 18:02:54 -0300

    feat(core): update next to v14 and repace npm for yarn

---
+++ src/components/formulario.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import gray from "@material-ui/core/colors/grey";
+import { Grid, TextField, makeStyles, Button } from "@material-ui/core";
+
+const style = makeStyles({
+  form: {
+    color: "#fff",
+    padding: "1.5em",
+  },
+  btn: {
+    margin: "25px 10px",
+    color: gray[50],
+  },
+});
+
+export default function Formulario() {
+  const formStyle = style();
+  const [data, setData] = useState({
+    marca: "",
+    modelo: "",
+    peca: "",
+    preço: "",
+  });
+
+  const Atualizar = (event) => {
+    const value = event.target.value;
+    const key = event.target.name;
+    setData((old) => ({
+      ...old,
+      [key]: value,
+    }));
+  };
+  const Cadastrar = async () => {
+    try {
+      const body = JSON.stringify(data);
+      await fetch("/api/cadastro_pecas", {
+        method: "POST",
+        body,
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  return (
+    <form className={formStyle.form}>
+      <fieldset>
+        <legend>Serviço Executado</legend>
+        <Grid item classes={{ root: formStyle.grid }} container spacing={1}>
+          <Grid>
+            <TextField
+              id="marca"
+              label="Marca do Aparelho"
+              variant="outlined"
+              onChange={Atualizar}
+              name="marca"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="modelo"
+              label="Modelo"
+              variant="outlined"
+              color="primary"
+              onChange={Atualizar}
+              name="modelo"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="peca"
+              label="Serviço efetuado"
+              variant="outlined"
+              color="primary"
+              onChange={Atualizar}
+              name="peca"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="preço"
+              label="Valor final"
+              variant="outlined"
+              color="primary"
+              onChange={Atualizar}
+              name="preço"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          variant="outlined"
+          color="primary"
+          className={style.btn}
+          onClick={Cadastrar}
+        >
+          Cadastrar
+        </Button>
+      </fieldset>
+    </form>
+  );
+}
